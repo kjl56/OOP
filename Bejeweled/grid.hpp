e07ebc829bef4a549a939e1a7d9e3b2bfcb5247e
Kyle Lugo <kjl56@zips.uakron.edu>
2019-03-04 23:44:34 -0500

    added comments

--- Bejeweled/grid.hpp
+++ Bejeweled/grid.hpp
@@ -6,10 +6,10 @@
 class Grid
 {
   public:
-    Grid(int ,int);
-    void fillGrid(int);
-    void swap(piece, piece);
-    piece& returnPiece(int, int);
+    Grid(int ,int); //creates empty grid with inputted dimensions
+    void fillGrid(int); //fills entire grid with random pieces
+    void swap(piece, piece); //swaps two pieces on the grid
+    piece& returnPiece(int, int); //returns reference to selected piece
 
   private:
     std::vector< std::vector<piece> > pieceGrid;
